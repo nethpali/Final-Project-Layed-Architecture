3f4700824cb2aaee08b25bec06aece1911ef014d
Nethpali_Jayaweera <nethpalijayaweera@gmail.com>
2023-06-16 18:52:08 +0530

    6th commit

--- src/main/java/lk/ijse/Roosalu/dao/Custom/ProductDAO.java
+++ src/main/java/lk/ijse/Roosalu/dao/Custom/ProductDAO.java
@@ -2,6 +2,8 @@ package lk.ijse.Roosalu.dao.Custom;
 
 import lk.ijse.Roosalu.Entity.Product;
 import lk.ijse.Roosalu.dao.CrudDAO;
+import lk.ijse.Roosalu.dto.OrderDto;
 
 public interface ProductDAO extends CrudDAO<Product> {
+    boolean saveOrder(OrderDto order);
 }
